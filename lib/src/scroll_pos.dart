631a50ad854b3864a65eb727ecbae35fb3d59eef
WinXaito <WinXaito@users.noreply.github.com>
2021-12-18 15:13:05 +0100

    Add code comments

--- lib/src/scroll_pos.dart
+++ lib/src/scroll_pos.dart
@@ -3,6 +3,11 @@ import 'package:flutter/widgets.dart';
 const _kDefaultAnimationDuration = Duration(milliseconds: 300);
 const _kDefaultAnimationCurve = Curves.ease;
 
+/// Controls a scrollable widget.
+///
+/// This class extends [ScrollController] and adds functionality to it.
+/// A ScrollPosController allow you to place an item on a specific area
+/// of the screen.
 class ScrollPosController extends ScrollController {
   int itemCount;
   bool animate;
@@ -23,45 +28,69 @@ class ScrollPosController extends ScrollController {
           debugLabel: debugLabel,
         );
 
-  double get inside => position.extentInside;
+  /// total returns the total of the inside part and the scrollable part.
+  double get total => position.extentInside + position.maxScrollExtent;
 
-  double get max => position.maxScrollExtent;
-
-  double get total => inside + max;
-
+  /// scrollPerItem returns the quantity of scroll needed for travel
+  /// the height of one item.
   double get scrollPerItem => total / itemCount;
 
-  double get visibleItems => inside / scrollPerItem;
+  /// visibleItems returns the quantity of items visible in the screen.
+  double get visibleItems => position.extentInside / scrollPerItem;
 
+  /// scrollTop will move the scrollbar to the top (first item).
   void scrollTop({bool? animate}) {
     _scrollToPos(0, animate: animate);
   }
 
+  /// scrollBottom will move the scrollbar to the bottom (last item).
   void scrollBottom({bool? animate}) {
-    _scrollToPos(max, animate: animate);
+    _scrollToPos(position.maxScrollExtent, animate: animate);
   }
 
+  /// scrollToItem will move the scrollbar to make the item
+  /// (defined by its index) visible on the screen.
+  /// If the item is already fully visible in the screen, the scrollbar
+  /// is not moved.
   void scrollToItem(int index, {bool? animate, bool center = false}) {
     _scrollOffItem(index, animate: animate, center: center);
+  }
+
+  /// scrollOffItemCenter return the offset when the item is placed at
+  /// the center of the screen.
+  double scrollOffItemCenter(int index) {
+    return (index + 1) * scrollPerItem - position.extentInside / 2;
+  }
+
+  /// scrollOfItemBottom return the offset when the item is placed at
+  /// the bottom of the screen.
+  double scrollOffItemBottom(int index) {
+    return (index + 1) * scrollPerItem - position.extentInside;
+  }
+
+  /// scrollOffItemTop return the offset when the item is placed at
+  /// the top of the screen.
+  double scrollOffItemTop(int index) {
+    return index * scrollPerItem;
   }
 
   void _scrollToPos(double offset, {bool? animate}) {
     if (this.animate ||
         (animate ?? false) && animationDuration != Duration.zero) {
       animateTo(
-        offset.clamp(0, max),
+        offset.clamp(0, position.maxScrollExtent),
         duration: animationDuration,
         curve: animationCurve,
       );
     } else {
-      jumpTo(offset.clamp(0, max));
+      jumpTo(offset.clamp(0, position.maxScrollExtent));
     }
   }
 
   void _scrollOffItem(int index, {bool? animate, bool center = false}) {
-    final centerVal = _scrollOffItemCenter(index);
-    final bottomVal = _scrollOffItemBottom(index);
-    final topVal = _scrollOffItemTop(index);
+    final centerVal = scrollOffItemCenter(index);
+    final bottomVal = scrollOffItemBottom(index);
+    final topVal = scrollOffItemTop(index);
 
     if (center) {
       _scrollToPos(centerVal, animate: animate);
@@ -70,17 +99,5 @@ class ScrollPosController extends ScrollController {
     } else if (offset > topVal) {
       _scrollToPos(topVal, animate: animate);
     }
-  }
-
-  double _scrollOffItemCenter(int index) {
-    return (index + 1) * scrollPerItem - inside / 2;
-  }
-
-  double _scrollOffItemBottom(int index) {
-    return (index + 1) * scrollPerItem - inside;
-  }
-
-  double _scrollOffItemTop(int index) {
-    return index * scrollPerItem;
   }
 }
